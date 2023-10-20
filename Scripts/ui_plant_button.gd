3546952ad17b90ebfeb5a137eca40c23e03b4ec6
Max Wiegel <max@w01.de>
2023-10-20 14:14:30 +0200

    Now sends an preview picture to the info overlay

--- Scripts/ui_plant_button.gd
+++ Scripts/ui_plant_button.gd
@@ -2,11 +2,17 @@ extends Button
 
 var plant_name
 
+var info_window = null
+
+func set_info_window(window):
+	info_window = window
+
 func set_plant(plant : String):
 	plant_name = plant
 
-
-
-
 func _on_pressed():
 	Plants.set_selected_plant(plant_name)
+	if info_window != null:
+		info_window.set_preview_picture(Plants.plantData[plant_name]["Icon"])
+
+
